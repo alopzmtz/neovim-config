79b408dc6c2c58e7a396a07ae04b6623b3d82828
Alejandro Lopez <alejandro@alopezmtz.com>
2024-04-03 16:56:45 -0600

    Change coloscheme to catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,46 +1,58 @@
 return {
-	{
-		"Mofiqul/dracula.nvim",
-		name = "dracula",
-		priority = 1000,
-		config = function()
-			require('dracula').setup({
-				colors = {
-					bg = "#22212C",
-					fg = "#F8F8F2",
-					selection = "#414D58",
-					comment = "#708CA9",
+	"catppuccin/nvim", 
+	name = "catppuccin", 
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			-- flavour = "auto" -- will respect terminal's background
+			background = { -- :h background
+			light = "latte",
+			dark = "mocha",
+		},
+		transparent_background = false, -- disables setting the background color.
+		show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+		term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+		dim_inactive = {
+			enabled = false, -- dims the background color of inactive window
+			shade = "dark",
+			percentage = 0.15, -- percentage of the shade to apply to the inactive window
+		},
+		no_italic = false, -- Force no italic
+		no_bold = false, -- Force no bold
+		no_underline = false, -- Force no underline
+		styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = { "italic" }, -- Change the style of comments
+		conditionals = { "italic" },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+		-- miscs = {}, -- Uncomment to turn off hard-coded styles
+	},
+	color_overrides = {},
+	custom_highlights = {},
+	default_integrations = true,
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		treesitter = true,
+		notify = false,
+		mini = {
+			enabled = true,
+			indentscope_color = "",
+		},
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
+})
 
-					red = "#FF9580",
-					orange = "#FFCA80",
-					yellow = "#FFFF80",
-					green = "#8AFF80",
-					purple = "#9580FF",
-					cyan = "#80FFEA",
-					pink = "#FF80BF",
-
-					bright_red = "#FF6E6E",
-					bright_yellow = "#FFFFA5",
-					bright_blue = "#D6ACFF",
-					bright_magenta = "#FF92DF",
-					bright_cyan = "#A4FFFF",
-					bright_white = "#FFFFFF",
-					bright_green = "#69FF94",
-
-					menu = "#21222C",
-					visual = "#3E4452",
-					gutter_fg = "#4B5263",
-					nontext = "#3B4048",
-					white = "#F8F8F2",
-					black = "#191A21",
-				},
-
-				show_end_of_buffer = true,
-				transparent_bg = true,
-				lualine_bg_color = "#22212C"
-			})
-
-			vim.cmd.colorscheme('dracula')
-		end
-	}
+vim.cmd.colorscheme "catppuccin"
+	end
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "dracula-nvim",
+				theme = "catppuccin",
 				globalstatus = true
 			},
 			sections = {
