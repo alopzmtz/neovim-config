e71f5bb0833cd545ea152c2dd88046a7b694e562
Alejandro Lopez <alejandro@alopezmtz.com>
2024-04-10 00:12:39 -0600

    various changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-	"nvim-telescope/telescope.nvim", 
+	"nvim-telescope/telescope.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		require("telescope").setup({
@@ -8,10 +8,23 @@ return {
 				layout_strategy = "horizontal",
 				layout_config = {
 					horizontal = {
-						prompt_position = "top"
-					}
+						prompt_position = "top",
+					},
 				},
-			}
+			},
 		})
-	end
+
+		local opts = { silent = true, noremap = true }
+		vim.keymap.set("n", "<Leader>fb", "<CMD>Telescope buffers<CR>", opts)
+		vim.keymap.set("n", "<Leader>fg", "<CMD>Telescope live_grep<CR>", opts)
+		vim.keymap.set("n", "<Leader>fh", "<CMD>Telescope help_tags<CR>", opts)
+		vim.keymap.set("n", "<leader>fc", "<CMD>Telescope current_buffer_fuzzy_find<CR>", opts)
+		vim.keymap.set("n", "<Leader>fa", "<CMD>Telescope find_files follow=true no_ignore=true hidden=true<CR>", opts)
+		vim.keymap.set(
+			"n",
+			"<Leader>ff",
+			"<CMD>Telescope find_files follow=true no_ignore=false hidden=false<CR>",
+			opts
+		)
+	end,
 }
