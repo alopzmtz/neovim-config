d710d08fc5c4a7f20ee7b26380e62e932797dcb8
Alejandro Lopez <alejandro@alopezmtz.com>
2024-04-10 00:10:34 -0600

    Move telescope keybind to own plugin config

--- init.lua
+++ init.lua
@@ -54,18 +54,10 @@ vim.opt.foldenable = false
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
--- keybinds
+-- Non plugins keybinds
 local opts = { silent = true, noremap = true }
 
 vim.keymap.set("n", "<Leader>q", "<CMD>bd!<CR>", opts)
 vim.keymap.set("n", "<Leader>s", "<CMD>write!<CR>", opts)
-
-
-vim.keymap.set("n", "<Leader>fb", "<CMD>Telescope buffers<CR>", opts)
-vim.keymap.set("n", "<Leader>fg", "<CMD>Telescope live_grep<CR>", opts)
-vim.keymap.set("n", "<Leader>fh", "<CMD>Telescope help_tags<CR>", opts)
-vim.keymap.set("n", "<leader>fc", "<CMD>Telescope current_buffer_fuzzy_find<CR>", opts)
-vim.keymap.set("n", "<Leader>fa", "<CMD>Telescope find_files follow=true no_ignore=true hidden=true<CR>", opts)
-vim.keymap.set("n", "<Leader>ff", "<CMD>Telescope find_files follow=true no_ignore=false hidden=false<CR>", opts)
 
 vim.keymap.set("i", "jk", "<ESC>", opts)
