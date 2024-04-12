81f4dd0e249b8cb2d0e87057bde3842f071e103b
Alejandro Lopez <alejandro@alopezmtz.com>
2024-04-12 06:51:27 -0600

    Add tmux-navigator plugin

---
+++ lua/plugins/tmux-navigation.lua
@@ -0,0 +1,15 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateRight",
+    "Tmu NavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
