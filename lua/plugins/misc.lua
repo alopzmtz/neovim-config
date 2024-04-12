66ebab934812d07428f274967e721f1d33cf6276
Alejandro Lopez <alejandro@alopezmtz.com>
2024-04-12 07:17:30 -0600

    Join plugins without configuration on same file

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"tpope/vim-fugitive",
+		event = "VeryLazy",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufRead",
+		config = true,
+	},
+	{
+		"tpope/vim-sleuth",
+		event = "VeryLazy",
+	}
+}
