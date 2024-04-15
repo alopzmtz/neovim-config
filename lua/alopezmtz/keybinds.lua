5d66ece17e11d7b6e00fc0d28f231295b5b04f40
Alejandro Lopez <alejandro@alopezmtz.com>
2024-04-14 19:57:29 -0600

    Added vertical movement keybind

--- lua/alopezmtz/keybinds.lua
+++ lua/alopezmtz/keybinds.lua
@@ -5,3 +5,6 @@ vim.keymap.set("i", "jk", "<ESC>", opts)
 
 vim.keymap.set("n", "<Leader>bq", "<CMD>bd!<CR>", opts)
 vim.keymap.set("n", "<Leader>s", "<CMD>write!<CR>", opts)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
