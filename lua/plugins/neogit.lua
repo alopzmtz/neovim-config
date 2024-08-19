0617de5e716f7186bdf451815acf81265176c37e
Alejandro Lopez <alejandro@alopezmtz.com>
2024-08-18 20:32:53 -0600

    added neogit keymap

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -5,6 +5,16 @@ return {
 			"nvim-lua/plenary.nvim",
 			"sindrets/diffview.nvim"
 		},
+		keys = {
+			{
+				"<leader>gs",
+				function()
+					require("neogit").open()
+				end,
+				mode = "n",
+				desc = "Open Neogit"
+			}
+		},
 		config = function()
 			local neogit = require("neogit")
 
