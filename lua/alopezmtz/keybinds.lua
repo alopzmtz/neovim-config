5f7651ebd49e3cc967b54983d3842ed1938745d5
Alejandro Lopez <alejandro@alopezmtz.com>
2024-08-22 11:12:04 -0600

    WIP: no highlight search keybind2

--- lua/alopezmtz/keybinds.lua
+++ lua/alopezmtz/keybinds.lua
@@ -1,10 +1,10 @@
-
 local opts = { silent = true, noremap = true }
 -- Non plugins keybind
 vim.keymap.set("i", "jk", "<ESC>", opts)
 
 vim.keymap.set("n", "<Leader>bq", "<CMD>bd!<CR>", opts)
 vim.keymap.set("n", "<Leader>s", "<CMD>write!<CR>", opts)
+vim.keymap.set("n", "<Leader>nhl", "<CMD>nohl<CR>", opts)
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
