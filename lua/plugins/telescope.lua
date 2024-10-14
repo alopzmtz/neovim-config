1e6a1cecbb5eaeb609ebeeca57c52180a8cee65a
Alejandro Lopez <alejandro@alopezmtz.com>
2024-10-14 06:37:11 -0600

    add ui-select extension to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,90 +1,98 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{
-			"<leader>ff",
-			function()
-				require("telescope.builtin").find_files({})
-			end,
-			mode = "n",
-			desc = "Find Files",
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					require("telescope.builtin").find_files({})
+				end,
+				mode = "n",
+				desc = "Find Files",
+			},
+			{
+				"<leader>fb",
+				function()
+					require("telescope.builtin").buffers({})
+				end,
+				mode = "n",
+				desc = "Find Buffers",
+			},
+			{
+				"<leader>fg",
+				function()
+					require("telescope.builtin").live_grep({})
+				end,
+				mode = "n",
+				desc = "Find Grep",
+			},
+			{
+				"<leader>fh",
+				function()
+					require("telescope.builtin").help_tags({})
+				end,
+				mode = "n",
+				desc = "Find Help",
+			},
+			{
+				"<leader>fc",
+				function()
+					require("telescope.builtin").current_buffer_fuzzy_find({})
+				end,
+				mode = "n",
+				desc = "Find Current Buffer",
+			},
+			{
+				"<leader>fa",
+				function()
+					require("telescope.builtin").find_files({ follow = true, no_ignore = true, hidden = true })
+				end,
+				mode = "n",
+				desc = "Find All Files",
+			},
 		},
-		{
-			"<leader>fb",
-			function()
-				require("telescope.builtin").buffers({})
-			end,
-			mode = "n",
-			desc = "Find Buffers",
-		},
-		{
-			"<leader>fg",
-			function()
-				require("telescope.builtin").live_grep({})
-			end,
-			mode = "n",
-			desc = "Find Grep",
-		},
-		{
-			"<leader>fh",
-			function()
-				require("telescope.builtin").help_tags({})
-			end,
-			mode = "n",
-			desc = "Find Help",
-		},
-		{
-			"<leader>fc",
-			function()
-				require("telescope.builtin").current_buffer_fuzzy_find({})
-			end,
-			mode = "n",
-			desc = "Find Current Buffer",
-		},
-		{
-			"<leader>fa",
-			function()
-				require("telescope.builtin").find_files({ follow = true, no_ignore = true, hidden = true })
-			end,
-			mode = "n",
-			desc = "Find All Files",
-		},
-	},
-	config = function()
-		local telescope = require("telescope")
-		local telescopeConfig = require("telescope.config")
+		config = function()
+			local telescope = require("telescope")
+			local telescopeConfig = require("telescope.config")
 
-		-- Clone the default Telescope configuration
-		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+			local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
-		-- I want to search in hidden/dot files.
-		table.insert(vimgrep_arguments, "--hidden")
-		-- I don't want to search in the `.git` directory.
-		table.insert(vimgrep_arguments, "--glob")
-		table.insert(vimgrep_arguments, "!**/.git/*")
+			-- I want to search in hidden/dot files.
+			table.insert(vimgrep_arguments, "--hidden")
+			-- I don't want to search in the `.git` directory.
+			table.insert(vimgrep_arguments, "--glob")
+			table.insert(vimgrep_arguments, "!**/.git/*")
 
-		telescope.setup({
-			defaults = {
-				path_display = { "filename_first" },
-				sorting_strategy = "ascending",
-				layout_strategy = "horizontal",
-				layout_config = {
-					horizontal = {
-						prompt_position = "top",
+			telescope.setup({
+				defaults = {
+					path_display = { "filename_first" },
+					sorting_strategy = "ascending",
+					layout_strategy = "horizontal",
+					layout_config = {
+						horizontal = {
+							prompt_position = "top",
+						},
+					},
+
+					-- `hidden = true` is not supported in text grep commands.
+					vimgrep_arguments = vimgrep_arguments,
+				},
+
+				pickers = {
+					find_files = {
+						-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 					},
 				},
+			})
 
-				-- `hidden = true` is not supported in text grep commands.
-				vimgrep_arguments = vimgrep_arguments,
-			},
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 
-			pickers = {
-				find_files = {
-					-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-				},
-			},
-		})
-	end,
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		event = "VeryLazy",
+	},
 }
